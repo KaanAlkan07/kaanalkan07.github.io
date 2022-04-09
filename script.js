249ccb617176f9cf556bcc1135625feb61b8bb74
KaanAlkan07 <101794579+KaanAlkan07@users.noreply.github.com>
2022-04-09 07:29:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("Personal");
+    var y = document.getElementById("Work");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+      y.style.display = "block";
+    }
+}
